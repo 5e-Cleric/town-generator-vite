c07a01b5334a7457129b8c7afc6c77b2d43e5199
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2025-08-01 00:17:52 +0200

    adding assets

--- src/mapComponent/mapAssets.jsx
+++ src/mapComponent/mapAssets.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react';
+
+const roofAssets = [
+	'Rectangular Roof 4A.png',
+	'Rectangular Roof 4B.png',
+	'Rectangular Roof 4C.png',
+	'Rectangular Roof 4D.png',
+	'Rectangular Roof 4E.png',
+	'Rectangular Roof 4F.png',
+	'Rectangular Roof 4G.png',
+	'Rectangular Roof 4H.png',
+];
+
 function RenderMapAssets() {
 	return (
 		<aside className="sidebar">
@@ -8,7 +21,17 @@ function RenderMapAssets() {
 						<div className="trigger">
 							<h3>Roofs</h3>
 						</div>
-						<div className="box" data-load="roofs"></div>
+						<div className="box">
+							{roofAssets.map((filename) => (
+								<div key={filename}  className="asset">
+									<img src={`/assets/images/roofs/${filename}`} alt={filename} />
+									<label>
+										<p>Use asset:</p>
+										<input type="checkbox" />
+									</label>
+								</div>
+							))}
+						</div>
 					</div>
 				</li>
 				<li>
